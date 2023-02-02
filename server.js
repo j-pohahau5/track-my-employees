60f83b05c75bca1e075f6b2d72c8924d5f448e67
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-02 09:18:52 -0800

    added query selectors for table, need to see how they work

--- server.js
+++ server.js
@@ -11,20 +11,34 @@ const db = mysql.createConnection(
     console.log(`Connected to the movie_db database.`)
 );
 
-// simple query
+// To view the correct department
+// TODO:
 db.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+    'SELECT * FROM department',
+    ['Page', 45],
+    function(err, results) {
+      console.log(results);
+    }
+  );
+
+// To view the correct role
+// TODO:
+db.query(
+  'SELECT r.title, r.title, d.name AS department, r.salary FROM role AS r JOIN department AS d on d.id = r.department_id',
   function(err, results, fields) {
     console.log(results); // results contains rows returned by server
     console.log(fields); // fields contains extra meta data about results, if available
   }
 );
 
-// with placeholder
+// To view the correct employee
+// TODO: convert manager_id num to manager name
 db.query(
-  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  'SELECT e.first_name, e.last_name, r.title, d.name as department, r.salary, e.manager_id FROM employee AS e JOIN role AS r on r.id = e.role_id INNER JOIN department AS d on d.id = r.department_id',
   ['Page', 45],
   function(err, results) {
     console.log(results);
   }
 );
+
+;
