68e9938cca5aa762748c83be8cb1a10c029aad0f
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-01 22:34:55 -0800

    added prompts need to correctly apply them

--- index.js
+++ index.js
@@ -1,5 +1,150 @@
 const inquirer = require("inquirer");
 
+
+
+const addDepartment = () => {
+    return (inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the name of the department?",
+        }
+    ])
+    .then(function({name}) {
+        startsPrompts()
+
+    }       
+    )
+    )
+}
+
+const addRole = () => {
+    return (inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title of the role?",
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the role?",
+        },
+        {
+            name: "department_name",
+            type: "list",
+            message: "Which department does does the role belong to?",
+            choices: [
+              "Sales",
+              "Engineering",
+              "Finance",
+              "Legal",
+            ],
+        },
+    ])
+    .then(
+      startsPrompts()
+      )
+    )
+}
+
+const addEmployee = () => {
+    return (inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?",
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?",
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: [
+              "Sales Lead",
+              "Salesperson",
+              "Lead Engineer",
+              "Software Engineer",
+              "Account Manager",
+              "Accountant",
+              "Legal Team Lead",
+              "Lawyer",
+            ],
+        },
+        {
+            name: "manager",
+            type: "list",
+            message: "Who is the employee's employee's manager?",
+            choices: [
+              "None",
+              "John Smith",
+              "Mike Chan",
+              "Ashley Rodriguez",
+              "Kevin Tupik",
+              "Kunal Singh",
+              "Malia Brown",
+              "Sarah Lourd",
+              "Tom Allen",
+            ],
+        },
+    ])
+    .then(
+      startsPrompts()
+      )
+    )
+}
+
+const updateEmployeeRole = () => {
+    return (inquirer.prompt([
+        {
+            name: "employee_name",
+            type: "list",
+            message: "Whish employee's role would you like to update?",
+            choices: [
+              "John Smith",
+              "Mike Chan",
+              "Ashley Rodriguez",
+              "Kevin Tupik",
+              "Kunal Singh",
+              "Malia Brown",
+              "Sarah Lourd",
+              "Tom Allen",
+            ],
+        },
+        {
+            name: "employee_role",
+            type: "list",
+            message: "Which role do you want to assign the selected employee?",
+            choices: [
+              "Sales Lead",
+              "Salesperson",
+              "Lead Engineer",
+              "Software Engineer",
+              "Account Manager",
+              "Accountant",
+              "Legal Team Lead",
+              "Lawyer",
+            ],
+        },
+    ])
+    .then(
+      startsPrompts()
+      )
+    )
+}
+const viewDepartments = () => {
+    startsPrompts
+}
+const viewRoles = () => {
+    startsPrompts
+}
+const viewEmployees = () => {
+    startsPrompts
+}
 const startsPrompts = () => {
   return (inquirer.prompt([
     {
@@ -19,30 +164,35 @@ const startsPrompts = () => {
   ]).then((chosen) => {
         switch (chosen.selection) {
           case "View All Departments":
-            addEngineer();
+            viewDepartments();
             break;
           case "View All Roles":
-            addIntern();
+            viewRoles();
             break;
           case "View All Employees":
-            addIntern();
+            viewEmployees();
             break;
           case "Add Department":
-            addIntern();
+            addDepartment();
             break;
           case "Add Role":
-            addIntern();
+            addRole();
             break;
           case "Add Employee":
-            addIntern();
+            addEmployee();
             break;
           case "Update Employee Role":
-            addIntern();
+            updateEmployeeRole();
             break;
           default:
-            startHtml();
+            startsPrompts();
         }
       })
   );
 };
 
+function init(){
+    startsPrompts()
+}
+
+init()
---
+++ server.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the movie_db database.`)
+);
+
+// simple query
+db.query(
+  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
+// with placeholder
+db.query(
+  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  ['Page', 45],
+  function(err, results) {
+    console.log(results);
+  }
+);
